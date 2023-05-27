ee0a829cd2a309cc23167306841a3b6d7681932c
Kruti2673 <krutivadaliya01@gmail.com>
2023-05-27 15:34:34 +0530

    update

--- middleware/validation.js
+++ middleware/validation.js
@@ -4,7 +4,21 @@ const validation = async (req, res, next) => {
   } else {
     return res
       .status(500)
-      .send({ status: false, error: "length is less than 3" });
+      .send({ status: false, error: "firstName length is less than 3" });
+  }
+  if (req.body.name.lastName.length >= 3) {
+    next();
+  } else {
+    return res
+      .status(500)
+      .send({ status: false, error: "lastName length is less than 3" });
+  }
+  if (typeof active === "boolean") {
+    next();
+  } else {
+    return res
+      .status(500)
+      .send({ status: false, error: "Invalid type of activeField" });
   }
 };
 
